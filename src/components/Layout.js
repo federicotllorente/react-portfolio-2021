5ddd2d721c59a41064c63b045a35b25b81998bf5
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-03-10 10:33:54 +0100

    Header apparition fixed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import BurgerMenu from './BurgerMenu';
 import Header from './Header';
 
 function Layout(props) {
+    let location = useLocation();
     return (
         <React.Fragment>
             <BurgerMenu />
             <div className="wrapper">
-                {(window.location.pathname !== '/') && (
+                {(location.pathname !== '/') && (
                     <Header />
                 )}
                 {props.children}
