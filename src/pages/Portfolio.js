7cd4b193341301f7abca897d2358cde094b8acbd
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-03-12 14:52:24 +0100

    Now when the app fetches the data shows a loader when is loading and an error message when there's no data available

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import Project from '../components/Project';
 
@@ -6,24 +7,23 @@ import api from '../api';
 const api_url = 'http://localhost:3001/projects/';
 
 function Portfolio(props) {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState(null);
     const [data, setData] = useState([]);
 
     const fetchData = async (api_url) => {
-        setLoading(true);
         const response = await api(api_url);
         try {
             setLoading(false);
             setData(response);
         } catch (err) {
             setLoading(false);
-            setError(err);
+            // setError(err);
         }
     };
 
     useEffect(() => {
-        fetchData(api_url)
+        fetchData(api_url);
     }, []);
 
     // If the data is loading
@@ -32,10 +32,14 @@ function Portfolio(props) {
             <div className="loader"></div>
         );
     }
-    // If there's an error
-    if (error != null) {
+    // If there's no data
+    if (Object.keys(data).length === 0 && !loading) {
         return (
-            <div className="error_modal">{error}</div>
+            <div className="error">
+                <h2>Sorry! But there's an error</h2>
+                <p>We couldn't find any data. Please, try again later.</p>
+                <Link to="/">Return to the Homepage</Link>
+            </div>
         );
     }
     // If it's successfully fetched the data
